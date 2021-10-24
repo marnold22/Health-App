ea3fabf88ad128abadbb6583000c0eb2186899bf
Michael <michaelarnold615@gmail.com>
2021-10-24 08:44:02 -0700

    Initial commit

---
+++ Health-App/Health-App/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Health_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int passout=0;
+        private void Passout_Increment(object sender, EventArgs e)
+        {
+            passout++;
+            ((Button)sender).Text = $"You have passed out {passout} times.";
+            //pcounter.Text += $"{passout}";
+            
+        }
+
+        private void Passout_Decrement(object sender, EventArgs e)
+        {
+            passout--;
+            ((Button)sender).Text = $"You have passed out {passout} times.";
+            //pcounter.Text += $"{passout}";
+        }
+    }
+}
